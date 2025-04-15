3fbdd82bc615fd6e897c5b713d6bdf69270cc1a9
Dreamteamdbl <amazingyaan@gmail.com>
2025-04-16 00:18:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config(object):
     API_ID = int(os.environ.get("12475131"))
     API_HASH = os.environ.get("719171e38be5a1f500613837b79c536f")
     VIP_USER = os.environ.get('VIP_USERS', '').split(',')
-    VIP_USERS = [int(1714266885) for user_id in VIP_USER]
+    VIP_USERS = [int(6133985472) for user_id in VIP_USER]
